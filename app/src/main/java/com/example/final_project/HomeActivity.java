2a541aabac4f0ca2bb41ab559d20c83223b17706
BEOM-109 <boem1009@naver.com>
2023-12-24 22:21:49 +0900

    update beom

--- app/src/main/java/com/example/final_project/HomeActivity.java
+++ app/src/main/java/com/example/final_project/HomeActivity.java
@@ -1,18 +1,53 @@
 package com.example.final_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.final_project.databinding.ActivityHomeBinding;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class HomeActivity extends AppCompatActivity {
-    ActivityHomeBinding homeBindinging;
+    ActivityHomeBinding homeBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        homeBindinging = ActivityHomeBinding.inflate(getLayoutInflater());
-        setContentView(homeBindinging.getRoot());
+        homeBinding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(homeBinding.getRoot());
+
+
+        homeBinding.bottomMenu.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                switch (item.getItemId()){
+                    case R.id.home:
+                        selectedFragment = new HomeFragment();
+                        break;
+                    case R.id.gallery:
+                        selectedFragment = new HomeFragment();
+                        break;
+                    case R.id.setting:
+                        selectedFragment = new HomeFragment();
+                        break;
+                    case R.id.statistics:
+                        selectedFragment = new HomeFragment();
+                        break;
+
+                }
+
+                if(selectedFragment != null){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, selectedFragment);
+                }
+
+                return true;
+            }
+        });
     }
 }
